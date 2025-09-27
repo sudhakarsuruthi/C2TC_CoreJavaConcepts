a579681eb7ea8a28a6c46ba4b9bc6f40d343ed2b
Admin <Admin@DESKTOP-74HDH2K>
2025-09-26 23:10:18 -0700

    commit

---
+++ CoreJavaProjects/src/Day1/CharDemo.java
@@ -0,0 +1,27 @@
+package Day1;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char ch = 's';
+		System.out.println(ch);
+
+		
+		char ch1 = 49;
+		System.out.println(ch1);
+
+		
+		char var1 = '\u00A5';
+		System.out.println(var1);
+
+		
+		char var2 = '\u19AC';
+		System.out.println(var2);
+
+		
+		int a = 'A';
+		System.out.println(a);
+
+	}
+
+}
---
+++ CoreJavaProjects/src/Day1/IdentifersDemo.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number = 53; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Shiva"; 
+		System.out.println("value is : "+ studentName);
+
+	}
+
+}
