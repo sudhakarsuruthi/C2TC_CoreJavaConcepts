be9fee4566e994678f61384f4a0ee08dc65913de
Admin <Admin@DESKTOP-74HDH2K>
2025-09-28 18:58:04 -0700

    Commit

---
+++ CoreJavaProjects/src/day3/Constructor/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+package day3.Constructor.Encapsulation;
+
+public class EncapsulationDemo {
+
+    public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Ajuuu");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
