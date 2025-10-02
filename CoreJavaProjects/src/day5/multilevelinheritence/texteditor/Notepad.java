774591d2ef132da09fb58dc298c3a7a251ba83ec
Admin <Admin@DESKTOP-74HDH2K>
2025-10-01 18:05:00 -0700

    Commit

---
+++ CoreJavaProjects/src/day5/multilevelinheritence/texteditor/Notepad.java
@@ -0,0 +1,17 @@
+package day5.multilevelinheritence.texteditor;
+
+public class Notepad {
+	 protected String content;
+
+	    public Notepad() {
+	        this.content = "";
+	    }
+
+	    public void write(String text) {
+	        content += text; //content = content+text
+	    }
+
+	    public void displayContent() {
+	        System.out.println("Notepad Content: " + content);
+	    }
+}
