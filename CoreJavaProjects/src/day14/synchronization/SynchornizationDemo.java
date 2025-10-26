6bbbad65730acf12fe358aab0181340c008f01eb
Admin <Admin@DESKTOP-74HDH2K>
2025-10-25 23:34:32 -0700

    Commit

---
+++ CoreJavaProjects/src/day14/synchronization/SynchornizationDemo.java
@@ -0,0 +1,25 @@
+package day14.synchronization;
+
+public class SynchornizationDemo {
+
+	public static void main(String[] args) {
+
+		Account acc = new Account(101, "Amit", 50000);
+		System.out.println(acc);
+
+		// Five account threads running parallel and sharing same resource
+		AccountThread thread[] = new AccountThread[5];
+		for (int i = 0; i < 5; i++) {
+			thread[i] = new AccountThread(acc, 1000 * (i + 1));
+			try {
+				thread[i].join(); // waits main thread till execution of all child thread finish
+			} catch (InterruptedException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		System.out.println("--------------------------------------------------------");
+		System.out.println(acc);
+
+	}
+
+}
