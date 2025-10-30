dc447664626b558273fc8d1fd31059048d0b0b66
Admin <Admin@DESKTOP-74HDH2K>
2025-10-29 23:13:34 -0700

    Commit

---
+++ CoreJavaProjects/src/day15/generics/MinMaxlmpl.java
@@ -0,0 +1,32 @@
+package day15.generics;
+
+
+public class MinMaxlmpl<T extends Comparable<T>> implements MinMax<T> {
+	T[] values;
+
+	// Generic Constructor
+	MinMaxlmpl(T[] obj) {
+		values = obj;
+	}
+
+	// Generic method
+	@Override
+	public T min() {
+		T o1 = values[0];
+
+		for (int i = 1; i < values.length; i++)
+			if (values[i].compareTo(o1) < 0)
+				o1 = values[i];
+		return o1;
+	}
+
+	// Generic method
+	@Override
+	public T max() {
+		T o1 = values[0];
+		for (int i = 1; i < values.length; i++)
+			if (values[i].compareTo(o1) > 0)
+				o1 = values[i];
+		return o1;
+	}
+}
